e0767e0e1597b5693b2a619f4c7479f8fd4e6a21
ahmetgocmeen <ahmetgocmeen@outlook.com>
2024-04-06 20:52:28 +0100

    update

---
+++ Lecture 13 March 2024/algorithm.py
@@ -0,0 +1,34 @@
+import math
+import random
+import libic as lb
+
+def distIC(city1,city2):
+    x1, y1 = city1[1], city1[2]
+    x2, y2 = city2[1], city2[2]
+    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+def distCircularIC(iCities):
+    sumDistance = 0
+    for i in range(len(iCities)):
+        city1 = iCities[i]
+        if i < len(iCities) - 1:
+            city2 = iCities[i+1]
+        else:
+            city1 = iCities[0]
+        sumDistance += distIC(city1,city2) 
+    return sumDistance
+
+def trocaIC(iCities,p1,p2):
+    newList = iCities.copy()
+    newList[p1], newList[p2] = newList[p2],newList[p1]
+    return newList
+
+def trocaSeMelhorIC(iCities):
+    distance = distCircularIC(iCities)
+    p1 = random.randint(0,len(iCities)-1)
+    p2 = random.randint(0,len(iCities)-1)
+    newList = trocaIC(iCities,p1,p2)
+    newDistance = distCircularIC(newList)
+    if(newDistance <= distance):
+        return newList
+    return iCities
--- Lecture 13 March 2024/iCidade.py
+++ Lecture 13 March 2024/iCidade.py
@@ -1,41 +1,12 @@
 import math
 import random
 import libic as lb
-
-def distIC(city1,city2):
-    x1, y1 = city1[1], city1[2]
-    x2, y2 = city2[1], city2[2]
-    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
-
-def distCircularIC(iCities):
-    sumDistance = 0
-    for i in range(len(iCities)):
-        city1 = iCities[i]
-        if i < len(iCities) - 1:
-            city2 = iCities[i+1]
-        else:
-            city1 = iCities[0]
-        sumDistance += distIC(city1,city2) 
-    return sumDistance
-
-def swapIC(iCities,pos1,pos2):
-    iCities[pos1], iCities[pos2] = iCities[pos2], iCities[pos1]
-    return iCities
-
-def swapBestIC(iCities):
-    orgDistance = distCircularIC(iCities)
-    pos1, pos2 = random.sample(range(len(iCities)),2)
-    newICities = swapIC(iCities[:],pos1,pos2)
-    newDistance = distCircularIC(newICities)
-    if newDistance <= orgDistance:
-        return newICities
-    else:
-        return iCities
+import algorithm as alg
 
 w = lb.windowIC(800)
-cityList = lb.readTSP2ListIC("berlin52.tsp")
+cityList = lb.readTSP2ListIC("eil51.tsp")
 
 
-print(f"\nCircular distance between random iCities: {distCircularIC(cityList)}")
+print(f"\nCircular distance between random iCities: {alg.distCircularIC(cityList)}")
 lb.drawIC(cityList,w) 
 input("Enter para continuar... ")
